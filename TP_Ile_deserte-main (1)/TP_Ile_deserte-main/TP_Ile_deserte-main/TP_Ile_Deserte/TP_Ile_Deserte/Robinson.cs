fd64f538bdbd4828328a2a19b79bff28befb4097
jsabatier <101117679+jsabatier@users.noreply.github.com>
2022-03-07 14:59:29 +0100

    Add files via upload

---
+++ TP_Ile_deserte-main (1)/TP_Ile_deserte-main/TP_Ile_deserte-main/TP_Ile_Deserte/TP_Ile_Deserte/Robinson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Ile_Deserte
+{
+    internal class Robinson
+    {
+        private string _nom;
+    }
+}
